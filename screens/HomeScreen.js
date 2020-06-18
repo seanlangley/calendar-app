339adfc11d85c807f5091a614848349dbae2d825
Sean Langley <sean.langley@ghs.com>
2020-06-17 22:20:34 -0700

    Remove some automatically generated stuff

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -18,14 +18,6 @@ export default function HomeScreen() {
         />
       <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
         <View style={styles.welcomeContainer}>
-          <Image
-            source={
-              __DEV__
-                ? require('../assets/images/robot-dev.png')
-                : require('../assets/images/robot-prod.png')
-            }
-            style={styles.welcomeImage}
-          />
         </View>
 
         <View style={styles.getStartedContainer}>
