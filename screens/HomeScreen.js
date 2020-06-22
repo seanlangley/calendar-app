06378e0d620fa1e10513f10460865e744efd0185
Sean Langley <sean.langley@ghs.com>
2020-06-21 21:14:33 -0700

    Get rid of unneeded code

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -7,9 +7,6 @@ import { MonoText } from '../components/StyledText';
 
 export default function HomeScreen() {
   new_data = []
-  DATA.forEach(element => {
-    new_data.push({key: element.day + " " + element.was_done});
-  });
   return (
     <View style={styles.container}>
         <FlatList
@@ -17,37 +14,10 @@ export default function HomeScreen() {
         renderItem={({item}) => <Text>{item.key}</Text>}
         />
       <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-        <View style={styles.welcomeContainer}>
-        </View>
-
         <View style={styles.getStartedContainer}>
           <DevelopmentModeNotice />
-
-          <Text style={styles.getStartedText}>Open up the code for this screen:</Text>
-
-          <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-            <MonoText>screens/HomeScreen.js</MonoText>
-          </View>
-
-          <Text style={styles.getStartedText}>
-            Change any of the text, save the file, and your app will automatically reload.
-          </Text>
-        </View>
-
-        <View style={styles.helpContainer}>
-          <TouchableOpacity onPress={handleHelpPress} style={styles.helpLink}>
-            <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
-          </TouchableOpacity>
         </View>
       </ScrollView>
-
-      <View style={styles.tabBarInfoContainer}>
-        <Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>
-
-        <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-          <MonoText style={styles.codeHighlightText}>navigation/BottomTabNavigator.js</MonoText>
-        </View>
-      </View>
     </View>
   );
 }
@@ -92,12 +62,6 @@ function handleLearnMorePress() {
   WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/workflow/development-mode/');
 }
 
-function handleHelpPress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/get-started/create-a-new-app/#making-your-first-change'
-  );
-}
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -113,139 +77,8 @@ const styles = StyleSheet.create({
   contentContainer: {
     paddingTop: 30,
   },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
   getStartedContainer: {
     alignItems: 'center',
     marginHorizontal: 50,
   },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
 });
-
-var DATA = [
-  {
-      "day": "2020-01-12",
-      "was_done": false,
-      "user": "http://localhost:8000/users/1/",
-      "name": "workout"
-  },
-  {
-      "day": "2020-01-19",
-      "was_done": false,
-      "user": "http://localhost:8000/users/1/",
-      "name": "workout"
-  },
-  {
-      "day": "2020-01-26",
-      "was_done": false,
-      "user": "http://localhost:8000/users/1/",
-      "name": "workout"
-  },
-  {
-      "day": "2020-01-28",
-      "was_done": true,
-      "user": "http://localhost:8000/users/1/",
-      "name": "workout"
-  },
-  {
-      "day": "2020-01-29",
-      "was_done": true,
-      "user": "http://localhost:8000/users/1/",
-      "name": "workout"
-  },
-  {
-      "day": "2020-01-31",
-      "was_done": true,
-      "user": "http://localhost:8000/users/1/",
-      "name": "workout"
-  },
-  {
-      "day": "2020-01-06",
-      "was_done": false,
-      "user": "http://localhost:8000/users/1/",
-      "name": "workout"
-  },
-  {
-      "day": "2020-01-08",
-      "was_done": false,
-      "user": "http://localhost:8000/users/1/",
-      "name": "workout"
-  },
-  {
-      "day": "2020-01-09",
-      "was_done": false,
-      "user": "http://localhost:8000/users/1/",
-      "name": "workout"
-  },
-  {
-      "day": "2020-01-10",
-      "was_done": true,
-      "user": "http://localhost:8000/users/1/",
-      "name": "workout"
-  }
-];
