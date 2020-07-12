abb1c4e37fd608db6414da3223ecf67d0a0c4246
Sean Langley <sean.langley@ghs.com>
2020-07-12 15:09:13 -0700

    Make just one function for the week data chart

--- screens/ActDetailScreen.tsx
+++ screens/ActDetailScreen.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import * as native from 'react-native';
-import { MonthChart, WeekChart } from '../components/actChart';
+import { MonthChart, WeekChart, chart_data } from '../components/actChart';
 import { ScrollView } from 'react-native-gesture-handler';
 import * as redux from 'react-redux';
 import { mapStateToProps } from '../redux/react_funcs';
@@ -9,25 +9,26 @@ import { root_state, activity_dict } from '../redux/reducers'
 const moment = require('moment');
 moment.locale('en');
 
-interface chart_data {
-    index: number;
-    value: number;
-}
 
 function ActDetailScreen(props: root_state) {
-    const [month_data, set_month_data] = useState<chart_data[]>([]);
-    const [week_data, set_week_data] = useState<chart_data[]>([]);
+    const [monthData, set_month_data] = useState<chart_data[]>([]);
+    const [weekData, set_week_data] = useState<chart_data[]>([]);
+    const [weekNumbers, set_week_numbers] = useState<chart_data[]>([]);
 
     useEffect(() => {
         let acts = props.actTypes[props.currActType].acts;
-        set_month_data(get_month_ratios(acts));
-        set_week_data(get_week_ratios(acts));
+        let month_data = get_month_ratios(acts);
+        let week_data = get_week_data(acts);
+        set_month_data(month_data);
+        set_week_data(week_data.boolean_ratios);
+        set_week_numbers(week_data.number_done);
     }, [props.actTypes[props.currActType].acts]);
     return (
         <native.View>
             <ScrollView>
-                <MonthChart data={month_data} />
-                <WeekChart data={week_data} />
+                <MonthChart data={monthData} />
+                <WeekChart data={weekData} />
+                <WeekChart data={weekNumbers} />
             </ScrollView>
         </native.View>
     );
@@ -60,15 +61,27 @@ function get_month_ratios(acts: activity_dict): chart_data[] {
     return monthdays_ratios;
 }
 
-function get_week_ratios(acts: activity_dict): chart_data[] {
-    var weekdays_true = Array(7).fill(0, 0, 7);
-    var weekdays_recorded = Array(7).fill(0, 0, 7);
-    var weekdays_ratios = Array(7).fill(0, 0, 7);
+interface week_data_t {
+    boolean_ratios: chart_data[];
+    number_done: chart_data[];
+}
+
+function get_week_data(acts: activity_dict): week_data_t {
+    let weekdays_true: number[] = Array(7).fill(0, 0, 7);
+    let weekdays_recorded: number[] = Array(7).fill(0, 0, 7);
+    let weekday_numbers: number[] = Array(7).fill(0,0,7);
+    let week_data: week_data_t = {
+        boolean_ratios: Array(7).fill(0,0,7),
+        number_done: Array(7).fill(0,0,7),
+    };
     Object.keys(acts).forEach(day => {
         let act = acts[day];
         let curr_day = moment(day);
         if (act.was_done) {
             weekdays_true[curr_day.day()] += 1;
+        }
+        if (act.was_done && Number.isInteger(act.number_done) && act.number_done != 0) {
+            weekday_numbers[curr_day.day()] += act.number_done;
         }
         weekdays_recorded[curr_day.day()] += 1;
     });
@@ -78,10 +91,14 @@ function get_week_ratios(acts: activity_dict): chart_data[] {
         if (ratio == Infinity || isNaN(ratio)) {
             ratio = 0;
         }
-        weekdays_ratios[i] = {
-            weekday: i,
-            ratio: ratio
+        week_data.boolean_ratios[i] = {
+            index: i,
+            value: ratio
         };
+        week_data.number_done[i] = {
+            index: i,
+            value: weekday_numbers[i]
+        }
     }
-    return weekdays_ratios;
+    return week_data;
 }
