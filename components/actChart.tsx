3e77eb5a1d596711cb37e88ff2b1872e6c4afb76
Sean Langley <sean.langley@ghs.com>
2020-07-12 14:53:16 -0700

    Organizing some code

--- components/actChart.tsx
+++ components/actChart.tsx
@@ -2,59 +2,70 @@ import React from 'react';
 import { VictoryBar, VictoryChart, VictoryAxis } from 'victory-native';
 import { View, Text } from 'react-native';
 
-const moment = require('moment');
-moment.locale('en');
-let months_as_str = moment.monthsShort();
-let weekdays_as_str = moment.weekdaysShort();
-var month_indices = [];
-var week_indices = [];
-for (var i = 0; i < 12; i++){
-    month_indices.push(i);
-}
-for (var i = 0; i < 7; i++){
-    week_indices.push(i);
+export interface chart_data {
+    index: number;
+    value: number;
 }
 
-export function MonthChart(props){
+
+interface props {
+    data: chart_data[];
+}
+
+export function MonthChart(props: props) {
+    const moment = require('moment');
+    moment.locale('en');
+    let months_as_str = moment.monthsShort();
+    var month_indices: number[] = [];
+    for (var i = 0; i < 12; i++) {
+        month_indices.push(i);
+    }
     return (
         <View>
             <VictoryChart>
-            <VictoryAxis
-            tickValues={month_indices}
-            tickFormat={months_as_str}
-            />
-            <VictoryAxis
-            dependentAxis
-            tickFormat={x => (x)}
-            />
-            <VictoryBar
-            data={props.data}
-            x='month'
-            y='ratio'
-            />
+                <VictoryAxis
+                    tickValues={month_indices}
+                    tickFormat={months_as_str}
+                />
+                <VictoryAxis
+                    dependentAxis
+                    tickFormat={x => (x)}
+                />
+                <VictoryBar
+                    data={props.data}
+                    x='index'
+                    y='value'
+                />
             </VictoryChart>
         </View>
     );
 }
 
-export function WeekChart(props){
+export function WeekChart(props: props) {
+    const moment = require('moment');
+    moment.locale('en');
+    let weekdays_as_str = moment.weekdaysShort();
+    var week_indices: number[] = [];
+    for (var i = 0; i < 7; i++) {
+        week_indices.push(i);
+    }
     return (
         <View>
             <VictoryChart>
-            <VictoryAxis
-            tickValues={week_indices}
-            tickFormat={weekdays_as_str}
-            />
-            <VictoryAxis
-            dependentAxis
-            tickFormat={x => (x)}
-            />
-            <VictoryBar
-            data={props.data}
-            x='weekday'
-            y='ratio'
-            />
-            </VictoryChart> 
+                <VictoryAxis
+                    tickValues={week_indices}
+                    tickFormat={weekdays_as_str}
+                />
+                <VictoryAxis
+                    dependentAxis
+                    tickFormat={x => (x)}
+                />
+                <VictoryBar
+                    data={props.data}
+                    x='index'
+                    y='value'
+                />
+            </VictoryChart>
         </View>
     );
 }
