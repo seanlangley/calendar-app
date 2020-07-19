400c20952b5c9600a0ad61335b997dcb95d954fb
Sean Langley <sean.langley@ghs.com>
2020-07-19 11:47:34 -0700

    Align the edit screen

--- screens/EditScreen.tsx
+++ screens/EditScreen.tsx
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import * as native from 'react-native';
+import { View } from 'react-native';
 import { mapStateToProps } from '../redux/react_funcs';
 import * as actions from '../redux/actions';
+
+let styles_g = require('../constants/styles');
 
 export function EditScreen(props: any) {
     const [units, setUnits] = useState<string>("");
@@ -24,33 +27,35 @@ export function EditScreen(props: any) {
                 value={name}
                 onChangeText={setName}
             />
-            <native.Button
-                title={"Submit"}
-                onPress={() => {
-                    if (name != "" || units != "") {
-                        props.dispatch(actions.editActType(name, props.currActType, units));
-                        props.dispatch(actions.setActType(name));
-                        props.navigation.goBack();
-                    }
-                }}
-            />
-            <native.Button
-                title={"Delete"}
-                onPress={() => {
-                    setDeleteText("This action is permament. Are you sure?");
-                    setYesText("Yes");
+            <View style={styles_g.leftAlign}>
+                <native.Button
+                    title={"Submit"}
+                    onPress={() => {
+                        if (name != "" || units != "") {
+                            props.dispatch(actions.editActType(name, props.currActType, units));
+                            props.dispatch(actions.setActType(name));
+                            props.navigation.goBack();
+                        }
+                    }}
+                />
+                <native.Button
+                    title={"Delete"}
+                    onPress={() => {
+                        setDeleteText("This action is permament. Are you sure?");
+                        setYesText("Yes");
 
-                }}
-            />
-            <native.Text style={styles.delete_text}>{deleteText}</native.Text>
-            <native.Button
-                title={yesText}
-                color={'red'}
-                onPress={() => {
-                    props.dispatch(actions.deleteActType(props.currActType))
-                    props.navigation.goBack();
-                }}
-            />
+                    }}
+                />
+                <native.Text style={styles.delete_text}>{deleteText}</native.Text>
+                <native.Button
+                    title={yesText}
+                    color={'red'}
+                    onPress={() => {
+                        props.dispatch(actions.deleteActType(props.currActType))
+                        props.navigation.goBack();
+                    }}
+                />
+            </View>
         </native.View>
     )
 }
